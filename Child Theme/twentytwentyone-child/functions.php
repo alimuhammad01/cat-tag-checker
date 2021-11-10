6ca5740c304447eb8b0bc56a192af2f452cf0aa4
alimuhammad01 <54906087+alimuhammad01@users.noreply.github.com>
2021-11-10 18:19:04 -0500

    Update functions.php

--- Child Theme/twentytwentyone-child/functions.php
+++ Child Theme/twentytwentyone-child/functions.php
@@ -5,4 +5,5 @@ function ctc_enqueue_scripts(){ //Enqueue all the files that required
 	wp_enqueue_style( 'twenty-twenty-one-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 	wp_enqueue_style( 'twenty-twenty-one-style-child', get_stylesheet_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 	wp_enqueue_style( 'pallete-mozaic-font', 'https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Palette+Mosaic&display=swap', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'oswald-font', 'https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap', array(), wp_get_theme()->get( 'Version' ) );
 }
